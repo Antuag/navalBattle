832a624e8b7571f76198303bdbe0f36f5b58300a
jacoboArroyave <jacoboArroyave>
2025-03-16 15:48:24 -0500

    Creacion del metodo loadPage

--- src/controllers/App.js
+++ src/controllers/App.js
@@ -1,3 +1,13 @@
-class App {
-
+class App { 
+    static async loadPage(url,container) {
+        const response = await fetch(url)
+        if (!response.ok) {
+            throw new Error("Failed to load page");
+        }
+        const html = await response.text();
+        if (container) {
+            container.innerHTML = html;
+        }
+    }
+    
 }
