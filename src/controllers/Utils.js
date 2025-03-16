6ddff3d928af7b5fae11fd0a587a68285ab365ca
jacoboArroyave <jacoboArroyave>
2025-03-16 16:02:25 -0500

    Actualizacion del metodo loadpage

--- src/controllers/Utils.js
+++ src/controllers/Utils.js
@@ -1,14 +1,20 @@
 class Utils
  { 
     static async loadPage(url,container) {
-        const response = await fetch(url)
-        if (!response.ok) {
-            throw new Error("Failed to load page");
+        try {
+            const response = await fetch(url)
+            if (!response.ok) {
+                throw new Error("Failed to load page");
+            }
+            const html = await response.text();
+            if (container) {
+                container.innerHTML = html;
+            }
         }
-        const html = await response.text();
-        if (container) {
-            container.innerHTML = html;
+        catch (error) {
+            console.error(error);
         }
+   
     }
     static async fetchJSON(url) {
         const response = await fetch(url)
