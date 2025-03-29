a9d5968056cf16c540ca0d7c8f1f15682c04157d
Antuag <joseguevaramur04@gmail.com>
2025-03-29 17:07:06 -0500

    Se crea tablero de juego, se inyecta por el momento en ranking, la funcion esta en opcionesjuego.js

--- src/controllers/opcionesJuego.js
+++ src/controllers/opcionesJuego.js
@@ -15,8 +15,45 @@ export async function crearTablero() {
       console.log(tablero);
       await Utils.loadPage("src/views/ranking.html", container, true);
       cargarPuntajes();
+      crearMapa();
     });
   } else {
     console.error("btnConfirmar presenta error");
+  }
+}
+
+export function crearMapa() {
+
+  const tablero = JSON.parse(localStorage.getItem("tablero"));
+  const tamaño = tablero["tamañoTablero"];
+
+  if (tamaño < 10 || tamaño > 20) {
+    alert("El tamaño del tablero debe ser mínimo de 10x10 y máximo de 20x20");
+    return;
+  }
+
+  const contenedor = document.getElementById("container");
+  if (!contenedor) {
+    console.error("No se encontró el elemento #container en la página.");
+    return;
+  }
+
+  for (let i = 0; i < tamaño; i++) {
+    let fila = document.createElement("div");
+    fila.classList.add("row");
+    fila.id = "fila-" + i;
+    contenedor.appendChild(fila);
+
+    for (let j = 0; j < tamaño; j++) {
+      let columna = document.createElement("div");
+      columna.classList.add("col-1");
+      columna.id = `columna-${j}`;
+
+      let casilla = document.createElement("button");
+      columna.appendChild(casilla);
+      casilla.id = `${i}-${j}`;
+      casilla.textContent = "";
+      fila.appendChild(columna);
+    }
   }
 }
